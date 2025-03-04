ccdc68addcb9e02890a23a91bd51cc08c26056c6
unknown <sanjeetkumarit@outlook.com>
2025-03-05 00:55:11 +0530

    Use gemini-1.5-pro Instead in India

--- src/components/ai-chatbot/ai-chatbot.tsx
+++ src/components/ai-chatbot/ai-chatbot.tsx
@@ -24,7 +24,7 @@ export class AiChatbot {
     //try-catch block to handle undefined API responses gracefully:
     try {
       const response = await fetch(
-        `https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent?key=${this.apiKey}`,
+        `https://generativelanguage.googleapis.com/v1/models/gemini-1.5-pro:generateContent?key=${this.apiKey}`,
         {
           method: "POST",
           headers: { "Content-Type": "application/json" },
